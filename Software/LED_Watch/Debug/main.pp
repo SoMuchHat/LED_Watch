97b6530d7063ea8590f8629f0321bd2ee0912b08
harrington.234 <harrington.234@osu.edu>
2015-08-21 00:09:34 -0400

    Started software. GPIO initialization setup.

---
+++ Software/LED_Watch/Debug/main.pp
@@ -0,0 +1,29 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430fr5721.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h
+main.obj: C:/Users/Sean/Documents/Electrical\ Projects/LED\ Watch/Software/LED_Watch/fram-utilities/ctpl/ctpl.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdint.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdbool.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/yvals.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/_lock.h
+
+../main.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430fr5721.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/intrinsics_legacy_undefs.h: 
+C:/Users/Sean/Documents/Electrical\ Projects/LED\ Watch/Software/LED_Watch/fram-utilities/ctpl/ctpl.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.3/include/_lock.h: 
---
+++ Software/LED_Watch/main.c
@@ -0,0 +1,46 @@
+#include <msp430.h>
+
+#include <ctpl.h>
+
+/*
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;    // Stop watchdog timer
+
+    initialize();
+    return 0;
+}
+
+void initialize()
+{
+
+	//GPIO Setup
+	//
+	// Data register setup for PxDIR, PxOUT
+	//   | Px.7 | Px.6 | Px.5 | Px.4 | Px.3 | Px.2 | Px.1 | Px.0 |
+	//
+
+	P1DIR = 0xFF; // All P1 ports outputs
+	P1OUT = 0x08; // P1.3 high, all else low
+
+	P2DIR = 0xBF; // P2.6 input, all else output
+	P2OUT = 0xB4; // P2.2, P2.4, P2.5, P2.7 high, all else low. P2.6 pulldown selected
+	P2REN |= BIT6; // Enable P2.6 pulldown
+	P2IES =~ BIT6; // Interrupt on low -> high
+	P2IE |= BIT6;  // Enable interrupt
+
+	P3DIR = 0xBF; // P3.6 input, all else output
+	P3OUT = 0x9B; // P3.1, P3.2, P3.3, P3.4, P3.7 high, all else low. P3.6 pulldown selected
+	P3REN |= BIT6; // Enable P3.6 pulldown
+	P3IES =~ BIT6; // Interrupt on low -> high
+	P3IE |= BIT6;  // Enable interrupt
+
+	P4DIR |= BIT0 + BIT1; // P4.0 and P4.1 outputs
+	P4OUT |= BIT1; // P4.1 high
+
+	// RTC Setup
+
+	PJSEL0 |= BIT4 + BIT5;
+}
